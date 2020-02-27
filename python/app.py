3904f281b8e7f8ded3dd77209b54e3b6cd459d1b
William <39922188+wilbobraggins@users.noreply.github.com>
2020-02-27 16:28:39 -0500

    testing automation  👷‍♂️

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Automate The Changes will work now because of a Spelling ERROR!"
+    return "Automate The Changes will work now because of a syntax ERROR! that I finally found"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
