307eba74576e327dc8b763bb5e9a23e91c8b20ed
William <39922188+wilbobraggins@users.noreply.github.com>
2020-02-27 16:22:18 -0500

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Automate The Changes Please"
+    return "Automate The Changes will work now because of a Spelling ERROR!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
