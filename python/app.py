e6b66d2bb96358964e7d1a6e3383919dee7d75e2
William <39922188+wilbobraggins@users.noreply.github.com>
2020-02-27 15:47:27 -0500

    test workflow automation

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Automate The Change"
+    return "Automate The Changes"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
